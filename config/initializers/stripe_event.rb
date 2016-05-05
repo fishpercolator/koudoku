8a7290756e1ae72b4cc3a2a36e2c9febba4b799e
Rich Daley <rich@fishpercolator.co.uk>
2016-05-05 10:41:45 +0100

    Add more logging to the Koudoku webhooks and don't die if subscriptions don't exist

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -1,27 +1,50 @@
 StripeEvent.configure do |events|
+  logger = Rails.logger
+  
   events.subscribe 'charge.failed' do |event|
     stripe_id = event.data.object['customer']
       
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.charge_failed
+    if subscription
+      logger.info "charge.failed webhook received for subscription #{subscription.id}"
+      subscription.charge_failed
+    else
+      logger.error "charge.failed webhook received for unknown subscription with Stripe ID #{stripe_id}"
+    end
   end
   
   events.subscribe 'invoice.payment_succeeded' do |event|
     stripe_id = event.data.object['customer']
     amount = event.data.object['total'].to_f / 100.0
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.payment_succeeded(amount)
+    if subscription
+      logger.info "invoice.payment_succeeded webhook received for subscription #{subscription.id}"
+      subscription.payment_succeeded(amount)
+    else
+      logger.error "invoice.payment_succeeded webhook received for unknown subscription with Stripe ID #{stripe_id}"
+    end
   end
   
   events.subscribe 'charge.dispute.created' do |event|
     stripe_id = event.data.object['customer']
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.charge_disputed
+    if subscription
+      logger.info "charge.dispute.created webhook received for subscription #{subscription.id}"
+      subscription.charge_disputed
+    else
+      logger.error "charge.dispute.created webhook received for unknown subscription with Stripe ID #{stripe_id}"
+    end
   end
   
   events.subscribe 'customer.subscription.deleted' do |event|
     stripe_id = event.data.object['customer']
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
-    subscription.subscription_owner.try(:cancel)
+    if subscription
+      logger.info "customer.subscription.deleted webhook received for subscription #{subscription.id}"
+      subscription.subscription_owner.try(:cancel)
+    else
+      # This is probably fine, if the event was triggered by deleting the subscription locally
+      logger.info "customer.subscription.deleted webhook received for subscription #{stripe_id} which looks like it has already been deleted"
+    end
   end
 end
